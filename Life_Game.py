26255e869fb4b6480914eea2add1835db74cce0f
d333pav <d333.pav@gmail.com>
2018-04-26 21:09:46 +0300

    Review_attemp3.2
    Полностью исправлены тесты, в функции Life() словарь теперь сам строится из всех предков класса Animal, универсализирована функция изменения океана.

--- Life_Game.py
+++ Life_Game.py
@@ -1,7 +1,12 @@
-from collections import OrderedDict
+import copy
 
 
-class Fish:
+class Animal:
+    pass
+
+
+class Fish(Animal):
+    symbol = 'f'
     nei_count = 0  # количество соседей
 
     def add_nei(self):  # функция добавления соседа
@@ -9,25 +14,46 @@ class Fish:
 
     def stay_alive(self):  # условия для продолжения жизни
         if self.nei_count == 2 or self.nei_count == 3:
-            return True
+            return self.symbol
         else:
-            return False
+            return 'n'
 
     def new_fish(self):  # условие появления новой рыбы
         if self.nei_count == 3:
-            return True
+            return self.symbol
         else:
-            return False
+            return 'n'
+
+    def action(self, elem='n', new_elem='n'):  # производим действие\
+        # основываясь на текущем элементе в океане и уже предложенным
+        if elem == 'n':
+            return self.new_fish()
+        elif elem == self.symbol:
+            return self.stay_alive()
 
 
 class Shrimp(Fish):
-    pass
+    symbol = 's'
+
+    def action(self, elem='n', new_elem='n'):
+        if elem == 'n' and new_elem == 'n':
+            return self.new_fish()
+        elif elem == self.symbol:
+            return self.stay_alive()
 
 
 def life(info_list=[0, 0, 0], ocean_array=[]):
     lines_number = info_list[0]  # количество строк в океане
     columns_number = info_list[1]  # количество столбцов
     turns_number = info_list[2]  # количество раз обновления океана
+    dict_fish = {}  # словарь для хранения классов
+
+    def sub_finder(obj):
+        for sub in obj.__subclasses__():
+            if dict_fish.get(sub.symbol) is None:
+                dict_fish[sub.symbol] = sub()
+                sub_finder(sub)
+    sub_finder(Animal)
     for turn in range(turns_number):
         new_ocean = [['n'] * columns_number for line in range(lines_number)]
         for x in range(lines_number):
@@ -35,9 +61,7 @@ def life(info_list=[0, 0, 0], ocean_array=[]):
                 if ocean_array[x][y] == 'r':
                     new_ocean[x][y] = 'r'
                 else:
-                    dict_fish = OrderedDict()
-                    dict_fish['s'] = Shrimp()
-                    dict_fish['f'] = Fish()
+                    current_dict = copy.deepcopy(dict_fish)
                     for nei_x in range(x-1, x+2):
                         for nei_y in range(y-1, y+2):
                             if (nei_x != x or nei_y != y)\
@@ -45,13 +69,13 @@ def life(info_list=[0, 0, 0], ocean_array=[]):
                                     and columns_number > nei_y > -1\
                                     and ocean_array[nei_x][nei_y] != 'n'\
                                     and ocean_array[nei_x][nei_y] != 'r':
-                                dict_fish[ocean_array[nei_x][nei_y]].add_nei()
-                    if ocean_array[x][y] == 'n':
-                        for fish in dict_fish:
-                            if dict_fish[fish].new_fish():
-                                new_ocean[x][y] = fish
-                    else:
-                        if dict_fish[ocean_array[x][y]].stay_alive():
-                            new_ocean[x][y] = ocean_array[x][y]
+                                current_dict[ocean_array[nei_x][nei_y]].\
+                                    add_nei()
+
+                    for fish in current_dict:
+                        new_element = current_dict[fish].\
+                            action(ocean_array[x][y], new_ocean[x][y])
+                        if new_element:
+                            new_ocean[x][y] = new_element
         ocean_array = new_ocean
     return ocean_array
--- Tests.py
+++ Tests.py
@@ -2,56 +2,48 @@ import unittest
 import Life_Game
 
 
-def life_test(name=''):
-    test_file = open('tests/test_'+name+'.txt', 'r')
-    first_line = True
+def life_test(test=''):
+    el_num = 0  # номер элемента
     info_list = []
     ocean_array = []
-    for line in test_file:
-        if first_line:  # считываем основные информацию только из первой строки
-            first_line = False
-            info_list = list(map(int, line.strip().split()))
+    for element in test.split():
+        if el_num < 3:  # считываем основные информацию только
+            # из первых трех элементов
+            info_list.append(int(element))
+            el_num += 1
         else:
-            line = line.replace('\n', '')
-            ocean_array.append(list(line))
-    test_file.close()
+            ocean_array.append(list(element))
     ocean_array = Life_Game.life(info_list, ocean_array)
     return ocean_array
 
 
-def fread_ans(name=''):
-    test_file = open('tests/test_' + name + '_ans.txt', 'r')
+def read_ans(ans):
     ocean_array = []
-    for line in test_file:
-        line = line.replace('\n', '')
+    for line in ans.split():
         ocean_array.append(list(line))
-    test_file.close()
     return ocean_array
 
 
 class TestOcean(unittest.TestCase):
     def test_simple(self):
-        name = 'simple'
-        test_ocean = life_test(name)
-        expect_ocean = fread_ans(name)
+        test = '3 3 1 nff fff fff'
+        ans = 'fnf nnn fnf'
+        test_ocean = life_test(test)
+        expect_ocean = read_ans(ans)
         self.assertEqual(test_ocean, expect_ocean)
 
     def test_order(self):
-        name = 'order'
-        test_ocean = life_test(name)
-        expect_ocean = fread_ans(name)
+        test = '3 3 1 fff nnn sss'
+        ans = 'nfn nfn nsn'
+        test_ocean = life_test(test)
+        expect_ocean = read_ans(ans)
         self.assertEqual(test_ocean, expect_ocean)
 
-    def test_big(self):
-        name = 'big'
-        test_ocean = life_test(name)
-        expect_ocean = fread_ans(name)
-        self.assertEqual(test_ocean, expect_ocean)
-
-    def test_iterations(self):
-        name = 'iterations'
-        test_ocean = life_test(name)
-        expect_ocean = fread_ans(name)
+    def test_rocks(self):
+        test = '3 3 1 rnr srf rnr'
+        ans = 'rnr nrn rnr'
+        test_ocean = life_test(test)
+        expect_ocean = read_ans(ans)
         self.assertEqual(test_ocean, expect_ocean)
 
 
